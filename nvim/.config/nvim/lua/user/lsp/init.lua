95938ddf1822adf7ac2e4ca2ef7400247a331479
dejor <71755747+dejor@users.noreply.github.com>
2024-03-20 17:17:38 +0100

    add autocmd for helm filetype

--- nvim/.config/nvim/lua/user/lsp/init.lua
+++ nvim/.config/nvim/lua/user/lsp/init.lua
@@ -3,6 +3,12 @@ if not status_ok then
   return
 end
 
+-- autocmd to set filtype helm
+vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {
+    pattern = '*/templates/*.yaml,*/templates/*.tpl,*.gotmpl,helmfile*.yaml',
+    callback = function() vim.opt_local.filetype = 'helm' end
+})
+
 require "user.lsp.mason"
 require("user.lsp.handlers").setup()
 require "user.lsp.null-ls"
