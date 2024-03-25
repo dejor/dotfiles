b032189838173e39e499684d94fa8bac91680ea2
dejor <71755747+dejor@users.noreply.github.com>
2024-03-25 20:13:07 +0100

    update tmux and add install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# tmux theme
+curl -o ~/.tmux.snazzy.theme https://raw.githubusercontent.com/ivnvxd/tmux-snazzy/main/.tmux.snazzy.theme
